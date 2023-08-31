94216617354851e6cc4cc23fffc2eaf696bb56c6
edu <eduardo.victorio@calcoastcu.org>
2023-08-30 20:23:36 -0500

    I Uploaded Changes Wednesday

--- src/main/java/steps/CalculatorSteps.java
+++ src/main/java/steps/CalculatorSteps.java
@@ -3,13 +3,23 @@ package steps;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import objects.CalculatorObject;
 
 public class CalculatorSteps {
+
+    private AndroidDriver<AndroidElement> driver;
+
+    private String numero1;
+
+    private CalculatorObject calculatorObject;
 
 
     @Given("el usuario tiene \"([^\"]*)\" numero and \"([^\"]*)\"")
     public void tashaHasTwoNumbers(Integer number1, Integer number2){
-
+            calculatorObject = new CalculatorObject(driver);
+            calculatorObject.touchNumber1();
     }
 
     @When("el usuario realiza la operacion de \"([^\"]*)\"")
--- src/test/resources/backyardigans/tasha.feature
+++ src/test/resources/backyardigans/tasha.feature
@@ -2,5 +2,5 @@ Feature: Calculadora
 
   Scenario: Operacion Matematica de Suma
     Given el usuario tiene "1" numero and "2"
-    When el usuario realiza la operacion de "suma"
-    Then el resultado de la operacion sera "3"
+    # When el usuario realiza la operacion de "suma"
+    # Then el resultado de la operacion sera "3"
